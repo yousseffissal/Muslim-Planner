d56c43cee99467002281341d75e72851acf50869
Youssef Fissal <youssefpaxar1@gmail.com>
2026-02-13 13:02:20 +0100

    Removed strictMode and used a routerProvider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import router from './routers/router.jsx'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
