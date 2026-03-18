fb52ac205bd34abc1ad8334209e68bbc47f16eb7
Youssef Fissal <youssefpaxar1@gmail.com>
2026-03-18 10:18:34 +0000

    Fixed the links

--- Frontend/src/tools/Links.js
+++ Frontend/src/tools/Links.js
@@ -1,10 +1,10 @@
 export const links = [
-    { name: 'Dashboard', icon: '📊', path: '/' },
-    { name: 'Prayer Times', icon: '🕌', path: '/prayer-times' },
-    { name: 'Quran', icon: '📖', path: '/quran' },
-    { name: 'My Schedule', icon: '🗓️', path: '/schedule' },
-    { name: 'Adkar', icon: '📿', path: '/adkar' },
-    { name: 'Tasbih', icon: '🤲', path: '/tasbih' },
-    { name: 'Calendar', icon: '🗓️', path: '/calendar' },
-    { name: 'About us', icon: 'ℹ️', path: '/about' },
+    { name: 'Dashboard', icon: '📊', path: '/app/dashboard' },
+    { name: 'Prayer Times', icon: '🕌', path: '/app/prayer-times' },
+    { name: 'Quran', icon: '📖', path: '/app/quran' },
+    { name: 'My Schedule', icon: '🗓️', path: '/app/schedule' },
+    { name: 'Adkar', icon: '📿', path: '/app/adkar' },
+    { name: 'Tasbih', icon: '🤲', path: '/app/tasbih' },
+    { name: 'Calendar', icon: '🗓️', path: '/app/calendar' },
+    { name: 'About us', icon: 'ℹ️', path: '/app/about' },
 ]
