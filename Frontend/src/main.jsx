87d5bc39bc757fdf21f1e647406d07b8504d9f75
Youssef Fissal <youssefpaxar1@gmail.com>
2026-03-18 10:17:11 +0000

    Added the auth provider to the main route

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,8 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './routers/router.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./routers/router.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
