380ad9e73607b22d81a1b48840bb08110e2b08a3
Youssef Fissal <youssefpaxar1@gmail.com>
2026-03-18 10:16:37 +0000

    Added user service controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -110,11 +110,38 @@ const deleteTask = async (req, res) => {
   }
 };
 
+const getMe = async (req, res) => {
+
+  try {
+
+    const user = await User
+      .findById(req.user.id)
+      .select("-password");
+
+    if (!user)
+      return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json(user);
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: "Server error",
+      error: error.message
+    });
+
+  }
+
+};
+
+module.exports = { getMe };
+
 module.exports = {
   updateQuranProgress,
   getQuranProgress,
   addTask,
   getTasks,
   updateTask,
-  deleteTask
+  deleteTask,
+  getMe
 };
