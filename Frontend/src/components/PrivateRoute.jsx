87810484004e6d1bd9775f18bbf2b2d8fb6acc14
Youssef Fissal <youssefpaxar1@gmail.com>
2026-03-18 19:21:57 +0000

    Added loading

--- Frontend/src/components/PrivateRoute.jsx
+++ Frontend/src/components/PrivateRoute.jsx
@@ -3,7 +3,19 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext.jsx";
 
 export default function PrivateRoute() {
-  const { isAuthenticated } = useAuth();
-  if (!isAuthenticated) return <Navigate to="/login" replace />;
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+
   return <Outlet />;
 }
