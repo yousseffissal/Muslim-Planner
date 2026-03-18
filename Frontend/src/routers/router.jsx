13110140ab19907a7c3599dbe9b15c96b687b6c0
Youssef Fissal <youssefpaxar1@gmail.com>
2026-03-18 10:19:00 +0000

    Added the new routes for the authentication and the privacy

--- Frontend/src/routers/router.jsx
+++ Frontend/src/routers/router.jsx
@@ -1,3 +1,4 @@
+// src/routers/router.jsx
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App.jsx";
 import AdhanTime from "../pages/AdhanTime.jsx";
@@ -5,31 +6,56 @@ import Welcome from "../pages/Welcome.jsx";
 import ErrorPage from "../pages/ErrorPage.jsx";
 import QuranPage from "../pages/QuranPage.jsx";
 import OwnerPage from "../pages/OwnerPage.jsx";
+import LandingPage from "../pages/Landing.jsx";
+import Login from "../pages/Login.jsx";
+import Register from "../pages/Register.jsx";
+import PrivateRoute from "../components/PrivateRoute.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <LandingPage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "login",
+    element: <Login />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "register",
+    element: <Register />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/app",
+    element: <PrivateRoute />,
     errorElement: <ErrorPage />,
     children: [
       {
-        index: true,
-        element: <Welcome />,
-      },
-      {
-        path: "prayer-times",
-        element: <AdhanTime />,
-      },
-      {
-        path: "quran",
-        element: <QuranPage />,
-      },
-      {
-        path: "about",
-        element: <OwnerPage />,
-      },
-    ],
-  },
+        path: "",
+        element: <App />,
+        children: [
+          {
+            path: "dashboard",
+            element: <Welcome />
+          },
+          {
+            path: "prayer-times",
+            element: <AdhanTime />
+          },
+          {
+            path: "quran",
+            element: <QuranPage />
+          },
+          {
+            path: "about",
+            element: <OwnerPage />
+          },
+        ]
+      }
+    ]
+  }
 ]);
 
 export default router;
