f79605abb9f0cf77a1489ed4f363d8a6d69d0b78
Youssef Fissal <youssefpaxar1@gmail.com>
2026-03-20 23:18:25 +0000

    Added theme colors for the dark mode

---
+++ Frontend/src/tools/theme.js
@@ -0,0 +1,52 @@
+export const themes = {
+    light: {
+        navbar: "#ffffff",
+        navbarlogo: "#16A34A",
+        navbarlinks: "#374151",
+        navbaractivelink: "#16A34A",
+
+        navbarmenu: "#16A34A",
+
+        navbarmobile: "#ffffff",
+        navbarmobilelinks: "#374151",
+        navbarmobileactivelink: "#ffffff",
+
+        scrollTrack: "#ffffff",
+        scrollThumb: "#16A34A",
+        scrollThumbHover: "#137738",
+
+        card: "#ffffff",
+        cardtext:"#374151",
+
+        quranpage: "#e6f9e6",
+        quranpagetext: "#000000",
+        quranpageayahtext: "#166534",
+        quranpageayahbg: "#BBF7D0",
+        quranpageayahplaybg: "#b6d6c1",
+    },
+    dark: {
+        navbar: "#1e1e1e",
+        navbarlogo: "#0ec451",
+        navbarlinks: "#ffffff",
+        navbaractivelink: "#0ec451",
+
+        navbarmenu: "#0ff765",
+
+        navbarmobile: "#2d2d30",
+        navbarmobilelinks: "#ffffff",
+        navbarmobileactivelink: "#ffffff",
+
+        scrollTrack: "#2d2d30",
+        scrollThumb: "#0ec451",
+        scrollThumbHover: "#137738",
+
+        card: "#303033",
+        cardtext:"#ffffff",
+
+        quranpage: "#131314",
+        quranpagetext: "#ffffff",
+        quranpageayahtext: "#07f763",
+        quranpageayahbg: "transparent",
+        quranpageayahplaybg: "#585e5a",
+    }
+};
