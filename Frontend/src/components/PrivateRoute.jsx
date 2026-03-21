fd075ce6f0b7cb4592c5205964efa79798353595
Youssef Fissal <youssefpaxar1@gmail.com>
2026-03-21 15:20:39 +0000

    Added theme handling to the loading page

--- Frontend/src/components/PrivateRoute.jsx
+++ Frontend/src/components/PrivateRoute.jsx
@@ -1,17 +1,22 @@
 // src/components/PrivateRoute.jsx
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext.jsx";
+import { useTheme } from "../context/ThemeContext";
 
 export default function PrivateRoute() {
   const { isAuthenticated, loading } = useAuth();
+   const { theme } = useTheme();
 
   if (loading) {
     return (
-      <div className="h-screen flex items-center justify-center bg-gray-100">
+      <div className="h-screen flex items-center justify-center"
+      style={{ background: theme.card }}>
         <div className="flex flex-col items-center">
           {/* Spinner */}
-          <div className="w-12 h-12 border-4 border-green-700 border-t-transparent rounded-full animate-spin mb-4"></div>
-          <p className="text-gray-700 font-medium text-lg">Loading...</p>
+          <div className="w-12 h-12 rounded-full animate-spin mb-4"
+          style={{border: `4px solid ${theme.navbarlogo}`,  borderTopColor: "transparent" }}></div>
+          <p className="font-medium text-lg"
+          style={{ color: theme.cardtext }}>Loading...</p>
         </div>
       </div>
     );
