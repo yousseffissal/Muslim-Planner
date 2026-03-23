ee2a1ed0d9d9e60f7b97244a3e5db6d6ea9f2d8e
Youssef Fissal <youssefpaxar1@gmail.com>
2026-03-23 04:30:47 +0100

    Added the readers data

---
+++ Frontend/src/tools/readers.js
@@ -0,0 +1,75 @@
+// src/data/readers.js
+export const arabicQuranReaders = [
+  {
+    identifier: "ar.alafasy",
+    name: "مشاري العفاسي",
+    englishName: "Mishary Alafasy",
+    url: "https://upload.wikimedia.org/wikipedia/commons/2/24/%D0%9C%D0%B8%D1%88%D0%B0%D1%80%D0%B8_%D0%A0%D0%B0%D1%88%D0%B8%D0%B4.jpg",
+  },
+  {
+    identifier: "ar.abdulbasitmurattal",
+    name: "عبد الباسط عبد الصمد المرتل",
+    englishName: "Abdul Basit (Murattal)",
+    url: " https://upload.wikimedia.org/wikipedia/ar/thumb/7/73/%D8%B5%D9%88%D8%B1%D8%A9_%D8%B4%D8%AE%D8%B5%D9%8A%D8%A9_%D8%B9%D8%A8%D8%AF_%D8%A7%D9%84%D8%A8%D8%A7%D8%B3%D8%B7_%D8%B9%D8%A8%D8%AF_%D8%A7%D9%84%D8%B5%D9%85%D8%AF.png/330px-%D8%B5%D9%88%D8%B1%D8%A9_%D8%B4%D8%AE%D8%B5%D9%8A%D8%A9_%D8%B9%D8%A8%D8%AF_%D8%A7%D9%84%D8%A8%D8%A7%D8%B3%D8%B7_%D8%B9%D8%A8%D8%AF_%D8%A7%D9%84%D8%B5%D9%85%D8%AF.png",
+  },
+  {
+    identifier: "ar.abdulsamad",
+    name: "عبد الباسط عبدالصمد",
+    englishName: "Abdul Samad",
+    url: "https://i1.sndcdn.com/artworks-000177208678-1wrabm-t500x500.jpg",
+  },
+  {
+    identifier: "ar.mahermuaiqly",
+    name: "ماهر المعيقلي",
+    englishName: "Maher Al Muaiqly",
+    url: "https://upload.wikimedia.org/wikipedia/commons/9/90/Maher_Al_Mueaqly.png",
+  },
+  {
+    identifier: "ar.saoodshuraym",
+    name: "سعود الشريم",
+    englishName: "Saood Al Shuraim",
+    url: "https://tecvitli.com/wp-content/uploads/2024/07/%D8%A7%D9%84%D8%B4%D9%8A%D8%AE-%D8%B3%D8%B9%D9%88%D8%AF-%D8%A7%D9%84%D8%B4%D8%B1%D9%8A%D9%85-1680073996-0.jpg",
+  },
+  {
+    identifier: "ar.minshawi",
+    name: "محمد صديق المنشاوي",
+    englishName: "Minshawi",
+    url: "https://media.gemini.media/img/medium/2017/6/2/2017_6_2_8_50_37_121.jpg",
+  },
+  {
+    identifier: "ar.husary",
+    name: "محمود خليل الحصري",
+    englishName: "Husary",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw8xs51ByjcyYCFe-g7wWZkzvxG0mymVhKwg&s",
+  },
+  {
+    identifier: "ar.husarymujawwad",
+    name: "محمود خليل الحصري (المجود)",
+    englishName: "Husary (Mujawwad)",
+    url: "https://static.suratmp3.com/pics/reciters/thumbs/23_600_600.jpg",
+  },
+  {
+    identifier: "ar.hudhaify",
+    name: "علي بن عبدالرحمن الحذيفي",
+    englishName: "Hudhaify",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFvpQkc0-eHlwI6y3n9yNg9KdW7c9fkzzDkw&s",
+  },
+  {
+    identifier: "ar.shaatree",
+    name: "أبو بكر الشاطري",
+    englishName: "Abu Bakr Ash-Shaatree",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs4GNujA0OpkSYuOQHnM8erbv1FZ6va2Dogw&s",
+  },
+  {
+    identifier: "ar.ahmedajamy",
+    name: "أحمد بن علي العجمي",
+    englishName: "Ahmed ibn Ali al-Ajamy",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR55-yMOest_e1VUXYu3uLn9sx8c5NzYrFuGg&s",
+  },
+  {
+    identifier: "ar.abdurrahmaansudais",
+    name: "عبدالرحمن السديس",
+    englishName: "Abdurrahmaan As-Sudais",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjwken58PrPJgdTkW950eo5HVEHz8h5QlCMQ&s",
+  },
+];
